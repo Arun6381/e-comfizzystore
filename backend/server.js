cc66af676c7da9e9411879702a941e9654852dbf
Arun6381 <kumararun28092@gmail.com>
2024-08-11 12:01:25 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,13 @@ app.use(
 );
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
